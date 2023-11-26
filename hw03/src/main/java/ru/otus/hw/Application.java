25be4c0d82101e892f805939a240e7eb0aaeb145
NPriimak <nikitapriymak@gmail.com>
2023-11-27 02:21:37 +0300

    hw03 done

--- hw03/src/main/java/ru/otus/hw/Application.java
+++ hw03/src/main/java/ru/otus/hw/Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.otus.hw.config.AppConfig;
-import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConfig.class)
@@ -12,10 +11,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        final var context = SpringApplication.run(Application.class, args);
-        //TODO: Make CLR
-        var testRunnerService = context.getBean(TestRunnerService.class);
-        testRunnerService.run();
+        SpringApplication.run(Application.class, args);
     }
 
 }
